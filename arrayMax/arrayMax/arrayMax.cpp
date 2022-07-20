9a3603768691fae11974290fd04f7a15a46d224e
LainaChinABK <lchin@activision.com>
2022-07-20 14:43:35 -0700

    arrayMax
    not sure when to use assert

---
+++ arrayMax/arrayMax/arrayMax.cpp
@@ -0,0 +1,55 @@
+// arrayMax.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+void populate(int arr[], int n);
+void display(int arr[], int n);
+int getMax(int arr[], int n);
+
+int main()
+{
+    const int n = 5;
+    int arr[n];
+
+    populate(arr, n);
+    display(arr, n);
+
+    int max = getMax(arr, n);
+    cout << "the biggest number in the array is " << max << endl;
+}
+
+void populate(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "index " << i << ": ";
+        cin >> arr[i];
+        assert(!cin.fail());
+    }
+}
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int getMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}

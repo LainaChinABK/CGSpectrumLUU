1c9249ace62fabd1a66f8e987e451ffda75d92ed
LainaChinABK <lchin@activision.com>
2022-07-20 10:35:41 -0700

    factors of a positive int

---
+++ factors/factors/factors.cpp
@@ -0,0 +1,37 @@
+// factors.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void factors(int n);
+
+int main()
+{
+    // initialize variable
+    int n = 0;
+
+    // get input from user
+    do
+    {
+        cout << "Please enter a positive integer: ";
+        cin >> n;
+    } while (n < 1);
+
+    factors(n);
+}
+
+void factors(int n)
+{
+    // loop through factors of n
+    for (int i = 1; i <= n; i++)
+    {
+        // check that i is a factor of n
+        if (n % i == 0)
+        {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+}

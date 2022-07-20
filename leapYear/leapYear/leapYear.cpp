639648f644c981eef051ce8c7b988aae613b71f8
LainaChinABK <lchin@activision.com>
2022-07-20 08:35:32 -0700

    leap year
    I haven't finished all of the videos for this section so I may go back and change a few things

---
+++ leapYear/leapYear/leapYear.cpp
@@ -0,0 +1,38 @@
+// leapYear.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// function declaration
+bool leap(int year);
+
+int main()
+{
+    // initialize to current year
+    int year = 2022;
+
+    // prompt user for year
+    cout << "What year is it? ";
+    cin >> year;
+
+    // call function and print result
+    if (leap(year))
+    {
+        cout << year << " is a leap year!" << endl;
+    }
+    else
+    {
+        cout << year << " is not a leap year." << endl;
+    }
+}
+
+// function definition
+bool leap(int year)
+{
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    {
+        return true;
+    }
+    return false;
+}

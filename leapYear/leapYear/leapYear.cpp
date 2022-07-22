8909a8a9830d3fe9048e62eda39e5fff89dd48b2
LainaChinABK <lchin@activision.com>
2022-07-22 15:36:23 -0700

    Update leapYear.cpp

--- leapYear/leapYear/leapYear.cpp
+++ leapYear/leapYear/leapYear.cpp
@@ -30,7 +30,7 @@ int main()
 // function definition
 bool leap(int year)
 {
-    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
     {
         return true;
     }

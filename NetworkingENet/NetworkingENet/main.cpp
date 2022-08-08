45e4b3b2ebbe358c4ed36bfc4f5fe5e872bb7697
LainaChinABK <lchin@activision.com>
2022-08-08 14:16:56 -0700

    consolidated into one project

--- NetworkingENet/NetworkingENet/main.cpp
+++ NetworkingENet/NetworkingENet/main.cpp
@@ -1,6 +1,13 @@
 #include <enet/enet.h>
 #include <iostream>
 
+ENetAddress address;
+ENetHost* server;
+ENetHost* client;
+
+bool CreateServer();
+bool CreateClient();
+
 int main(int argc, char** argv)
 {
     if (enet_initialize() != 0)
@@ -11,8 +18,51 @@ int main(int argc, char** argv)
     }
     atexit(enet_deinitialize);
 
-    ENetAddress address;
-    ENetHost* server;
+    std::cout << "1) Create server" << std::endl;
+    std::cout << "2) Create client" << std::endl;
+    
+    int input = 0;
+    std::cin >> input;
+
+    if (input == 1)
+    {
+        if (!CreateServer())
+        {
+            fprintf(stderr,
+                "An error occurred while trying to create an ENet server host.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (input == 2)
+    {
+        if (!CreateClient())
+        {
+            fprintf(stderr,
+                "An error occurred while trying to create an ENet client host.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        std::cout << "Invalid input!" << std::endl;
+    }
+        
+
+    if (server != nullptr)
+    {
+        enet_host_destroy(server);
+    }
+
+    if (client != nullptr)
+    {
+        enet_host_destroy(client);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+bool CreateServer()
+{
     /* Bind the server to the default localhost.     */
     /* A specific host address can be specified by   */
     /* enet_address_set_host (& address, "x.x.x.x"); */
@@ -24,15 +74,17 @@ int main(int argc, char** argv)
         2      /* allow up to 2 channels to be used, 0 and 1 */,
         0      /* assume any amount of incoming bandwidth */,
         0      /* assume any amount of outgoing bandwidth */);
-    
-    if (server == NULL)
-    {
-        fprintf(stderr,
-            "An error occurred while trying to create an ENet server host.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    enet_host_destroy(server);
 
-    return EXIT_SUCCESS;
+    return server != nullptr;
+}
+
+bool CreateClient()
+{
+    client = enet_host_create(NULL /* create a client host */,
+        1 /* only allow 1 outgoing connection */,
+        2 /* allow up 2 channels to be used, 0 and 1 */,
+        0 /* assume any amount of incoming bandwidth */,
+        0 /* assume any amount of outgoing bandwidth */);
+
+    return client != nullptr;
 }
